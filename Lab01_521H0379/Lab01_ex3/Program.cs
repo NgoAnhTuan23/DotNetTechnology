61df174ba8de5d90fa06a3f6b8f6c66859fba76b
NgoAnhTuan23 <521H0379@student.tdtu.edu.vn>
2023-09-22 00:17:45 +0700

    Ex3

---
+++ Lab01_521H0379/Lab01_ex3/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int n = random.Next(1, 100);
+        int c = 0;
+        Console.WriteLine("Let's guess the number (1 - 100): ");
+        do
+        {
+            if (int.TryParse(Console.ReadLine(), out int a))
+            {
+                if (a < n)
+                {
+                    Console.WriteLine("Too Low!!");
+                    c++;
+                }
+                else if (a > n)
+                {
+                    Console.WriteLine("Too High!!");
+                    c++;
+                }
+                else
+                {
+                    Console.WriteLine("Congratulation!!");
+                    c++;
+                    Console.WriteLine($"Number of times you guessed: {c}");
+                    break;
+                }
+            }
+            else Console.WriteLine("Error Number!!");
+        }
+        while (true);
+    }
+}
