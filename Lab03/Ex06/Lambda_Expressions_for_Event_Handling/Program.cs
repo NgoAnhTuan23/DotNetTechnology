c9b6459ec4c18358e4f7bfca90b797725c08b363
NgoAnhTuan23 <521H0379@student.tdtu.edu.vn>
2023-10-05 21:51:01 +0700

    Lab03_Ex06

---
+++ Lab03/Ex06/Lambda_Expressions_for_Event_Handling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// 1. Tạo một lớp tùy chỉnh với một sự kiện
+class Button
+{
+    // Định nghĩa một sự kiện Click
+    public event Action Click;
+
+    // Phương thức này gọi sự kiện Click khi nút được nhấp
+    public void OnClick()
+    {
+        Click?.Invoke();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // 2. Tạo một đối tượng Button
+        Button myButton = new Button();
+
+        // 3. Sử dụng lambda expression để đăng ký xử lý sự kiện Click
+        myButton.Click += () =>
+        {
+            Console.WriteLine("Button clicked: Success!!!");
+        };
+
+        // 4. Khi nút được nhấp, sự kiện Click được gọi và lambda expression được thực thi
+        myButton.OnClick();
+    }
+}
