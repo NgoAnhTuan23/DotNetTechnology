427f36ac78b2db10dbf9c2e048e90217ade5e6e2
NgoAnhTuan23 <521H0379@student.tdtu.edu.vn>
2023-10-05 20:45:10 +0700

    Lab03_Ex04

---
+++ Lab03/Ex04/Custom_Generic_List/Program.cs
@@ -0,0 +1,29 @@
+﻿using Custom_Generic_List;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomList<int> myList = new CustomList<int>();
+
+        myList.Add(23);
+        myList.Add(01);
+        myList.Add(2003);
+        myList.Add(14);
+        myList.Add(06);
+        myList.Add(13);
+        myList.Add(05);
+
+        Console.WriteLine("Elements in the list:");
+        myList.Print();
+        Console.WriteLine("========================================");
+
+        Console.WriteLine($"Element at index 1: {myList[5]}");
+        Console.WriteLine("========================================");
+
+        myList.Remove(2003);
+        Console.WriteLine("After removing 2003:");
+        myList.Print();
+        Console.WriteLine("========================================");
+
+    }
+}
